49a26853b454c63792760783468a61ecf6916e59
unisoftsystem <30917020+unisoftsystem@users.noreply.github.com>
2017-12-18 08:00:16 -0500

    20171218

--- protected/models/WalletsByCampaign.php
+++ protected/models/WalletsByCampaign.php
@@ -15,7 +15,7 @@ class WalletsByCampaign extends GxActiveRecord
 			':idCampaign' => $data['idCampaign'], 
 			':campaignName' => $data['campaignName'], 
 			':serviceType' => $data['serviceType'], 
-			':notificationType' => $data['notificationType']
+			':notificationType' => $data ['notificationType']
 		];
 		return Yii::app()->db->createCommand($sql)->execute($parameters);
 	}
@@ -28,6 +28,12 @@ class WalletsByCampaign extends GxActiveRecord
 	/*===========================================================================================================
     	11/Sept/2017 Unisoft - Wilmar Ibarguen M.
     ===========================================================================================================*/ 
+
+    public static function getWalletsByCampaign($id)
+    {
+    	$sql = sprintf('SELECT * FROM `wallets_by_campaign` WHERE `idWalletByCampaign` = "%s"', $id);
+    	return Yii::app()->db->createCommand($sql)->queryAll();
+    }
 
 	// Esta funcion devuelve el listado de campañas con:
 	// Campaña	Cliente	Fecha Creación	Tipo de servicio	
@@ -74,10 +80,38 @@ class WalletsByCampaign extends GxActiveRecord
 		return Yii::app()->db->createCommand($sql)->execute($parameters);
 	}
 
+	public static function getBalanceCampaing($id)
+	{
+		$sql = sprintf('SELECT campaigns.name, b.idCampaign, a.campaignName, SUM(b.capitalValue) as "saldo_asignado", COUNT(b.id) as total_deudores, COUNT(q1.idCampaign) as q1_deudores, COUNT(q2.idCampaign) as q2_deudores, COUNT(q3.idCampaign) as q3_deudores, COUNT(q4.idCampaign) as q4_deudores, COUNT(q5.idCampaign) as q5_deudores, COUNT(q6.idCampaign) as q6_deudores, COUNT(q7.idCampaign) as q7_deudores, ((SUM(c.value) / "saldo_asignado") * 100) as recuperacion, a.createAt FROM wallets_by_campaign a INNER JOIN wallets_tempo b ON b.idCampaign = a.idWalletByCampaign INNER JOIN payments c ON c.idPayment = b.id LEFT JOIN q1 ON q1.idCampaign = a.IdCampaign LEFT JOIN q2 ON q2.idCampaign = a.IdCampaign LEFT JOIN q3 ON q3.idCampaign = a.IdCampaign LEFT JOIN q4 ON q4.idCampaign = a.IdCampaign LEFT JOIN q5 ON q5.idCampaign = a.IdCampaign LEFT JOIN q6 ON q6.idCampaign = a.IdCampaign LEFT JOIN q7 ON q7.idCampaign = a.IdCampaign LEFT JOIN campaigns ON campaigns.idCampaign = a.IdCampaign WHERE a.IdCampaign = "%s" GROUP BY a.idWalletByCampaign', $id);
+
+		$res = Yii::app()->db->createCommand($sql)->queryAll();
+
+		foreach ($res as $key => $value) {
+			$sqlStatus = sprintf('SELECT legalName, idNumber, status.description, DATEDIFF(NOW(), wallets_by_campaign.createAt) AS mora, ((SUM(payments.value) / capitalValue) * 100) AS recuperacion FROM wallets_tempo INNER JOIN status ON status.idStatus = wallets_tempo.idStatus INNER JOIN payments ON payments.idPayment = wallets_tempo.id INNER JOIN wallets_by_campaign ON wallets_by_campaign.idWalletByCampaign = wallets_tempo.idCampaign WHERE wallets_tempo.idCampaign = "%s" GROUP BY wallets_tempo.id ORDER BY wallets_tempo.legalName', $value['idCampaign']);
+			$deudores = Yii::app()->db->createCommand($sqlStatus)->queryAll();
+			$res[$key]['deudores'] = ($deudores) ? $deudores : [];
+		}
+
+		return $res;
+	}
+
 	public static function getBalanceCampaings($id)
 	{
-		$sql = sprintf('SELECT a.campaignName, SUM(b.capitalValue) as "saldo_asignado", COUNT(b.id) as total_deudores, COUNT(q1.idCampaign) as q1_deudores, COUNT(q2.idCampaign) as q2_deudores, COUNT(q3.idCampaign) as q3_deudores, COUNT(q4.idCampaign) as q4_deudores, COUNT(q5.idCampaign) as q5_deudores, COUNT(q6.idCampaign) as q6_deudores, COUNT(q7.idCampaign) as q7_deudores, ((SUM(c.value) / "saldo_asignado") * 100) as recuperacion, a.createAt FROM wallets_by_campaign a INNER JOIN wallets_tempo b ON b.idCampaign = a.idWalletByCampaign INNER JOIN payments c ON c.idPayment = b.id LEFT JOIN q1 ON q1.idCampaign = a.IdCampaign LEFT JOIN q2 ON q2.idCampaign = a.IdCampaign LEFT JOIN q3 ON q3.idCampaign = a.IdCampaign LEFT JOIN q4 ON q4.idCampaign = a.IdCampaign LEFT JOIN q5 ON q5.idCampaign = a.IdCampaign LEFT JOIN q6 ON q6.idCampaign = a.IdCampaign LEFT JOIN q7 ON q7.idCampaign = a.IdCampaign WHERE a.IdCampaign = "%s" GROUP BY a.idWalletByCampaign', $id);
+		$sql = sprintf('SELECT campaigns.name, b.idCampaign, a.campaignName, SUM(b.capitalValue) as "saldo_asignado", COUNT(b.id) as total_deudores, COUNT(q1.idCampaign) as q1_deudores, COUNT(q2.idCampaign) as q2_deudores, COUNT(q3.idCampaign) as q3_deudores, COUNT(q4.idCampaign) as q4_deudores, COUNT(q5.idCampaign) as q5_deudores, COUNT(q6.idCampaign) as q6_deudores, COUNT(q7.idCampaign) as q7_deudores, ((SUM(c.value) / "saldo_asignado") * 100) as recuperacion, a.createAt FROM wallets_by_campaign a INNER JOIN wallets_tempo b ON b.idCampaign = a.idWalletByCampaign INNER JOIN payments c ON c.idPayment = b.id LEFT JOIN q1 ON q1.idCampaign = a.IdCampaign LEFT JOIN q2 ON q2.idCampaign = a.IdCampaign LEFT JOIN q3 ON q3.idCampaign = a.IdCampaign LEFT JOIN q4 ON q4.idCampaign = a.IdCampaign LEFT JOIN q5 ON q5.idCampaign = a.IdCampaign LEFT JOIN q6 ON q6.idCampaign = a.IdCampaign LEFT JOIN q7 ON q7.idCampaign = a.IdCampaign LEFT JOIN campaigns ON campaigns.idCampaign = a.IdCampaign WHERE a.IdCampaign = "%s" GROUP BY a.idWalletByCampaign', $id);
 
+		$res = Yii::app()->db->createCommand($sql)->queryAll();
+
+		foreach ($res as $key => $value) {
+			$sqlStatus = sprintf('SELECT status.description, COUNT(wallets_tempo.idStatus) AS cantidad FROM status INNER JOIN wallets_tempo on wallets_tempo.idStatus = status.idStatus WHERE wallets_tempo.idCampaign = "%s" GROUP BY wallets_tempo.idStatus ORDER BY status.description', $value['idCampaign']);
+			$res[$key]['estados'] = Yii::app()->db->createCommand($sqlStatus)->queryAll();
+		}
+
+		return $res;
+	}
+
+	public static function getCampainData($idCampaign)
+	{
+		$sql = sprintf('SELECT * FROM `campaigns` WHERE idCampaign = "%s"', $idCampaign);
 		return Yii::app()->db->createCommand($sql)->queryAll();
 	}
 }
